a8248742ebb974d03662289903ad842e3182e2b2
Perriesha <pmbyrd@gmail.com>
2023-02-09 12:27:35 -0800

    ComputerChoice func

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ console.log("Hello World");
 // ! I will be creating a game and the logic and steps written is the outline that I should attempt to follow
 // Create a function that will create a timer this will be used to trigger to toggle classes on the choices
 const choices = ["rock", "paper", "scissors"];
+const computerCount = 0;
+const playerCount = 0;
 
+
+console.log("Hello World");
 //TODO getComputerChoices()
+function getComputerChoices() {
+    const choice = getRandomNumber();
+    return choices[choice];
+}
 
 // TODO getRandomNumber()
 function getRandomNumber() {
     // the choices is a len of "2" so I need a number choice
-    return Math.floor(Math.random() * 3)
+    const choice =  Math.floor(Math.random() * choices.length);
+    return choice
 }
 
+getRandomNumber();
 // Todo playRound(arg1, arg2)
 // *This function handles the game logic if computer choice === ? write that logic
 //playerSelection()
