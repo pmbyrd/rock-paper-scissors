7f59a68ec869d097c8c190a094e0f7c52721bec3
Perriesha <pmbyrd@gmail.com>
2023-02-09 12:45:59 -0800

    Player choice input value is read

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-console.log("Hello World");
+
 // ! I will be creating a game and the logic and steps written is the outline that I should attempt to follow
 // Create a function that will create a timer this will be used to trigger to toggle classes on the choices
 const choices = ["rock", "paper", "scissors"];
 const computerCount = 0;
 const playerCount = 0;
+const $btn = $(".btn");
+const $playerInput = $("#player-input");
+console.log($btn, $playerInput);
 
 
-console.log("Hello World");
 //TODO getComputerChoices()
 function getComputerChoices() {
-    const choice = getRandomNumber();
-    return choices[choice];
+    const computerChoice = getRandomNumber();
+    return choices[computerChoice];
 }
 
+
 // TODO getRandomNumber()
-function getRandomNumber() {
-    // the choices is a len of "2" so I need a number choice
+function getRandomNumber() {  // get the choices array length as the the numerical value to be passed through the Math.random() function
     const choice =  Math.floor(Math.random() * choices.length);
     return choice
 }
 
-getRandomNumber();
+// *with the computer choice function working I can now work on getting the player choice
+// TODO getPlayerChoice()
+function getPlayerChoice() { //get the player choice value from the input field
+    const playerChoice = $playerInput.val();
+    playerChoice.toLowerCase(); // convert the player choice to lower case to avoid errors
+    return playerChoice;
+}
 // Todo playRound(arg1, arg2)
 // *This function handles the game logic if computer choice === ? write that logic
 //playerSelection()
